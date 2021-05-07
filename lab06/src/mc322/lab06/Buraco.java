fa5e877a6fe8c5af4961b8df04a5d4cf0219c010
José Felipe Theodoro <j219081@dac.unicamp.br>
2021-05-07 12:26:44 -0300

    Começo das classes Wumpus, Buraco, Brisa e Fedor

--- lab06/src/mc322/lab06/Buraco.java
+++ lab06/src/mc322/lab06/Buraco.java
@@ -1,5 +1,13 @@
 package mc322.lab06;
 
 public class Buraco extends Componente {
+    public static int prioridade = 4;
 
+    Buraco(String sprite, int score) {
+        super.Componentes("B", -1000);
+    }
+
+    public void gerarBrisa(Caverna caverna, int []pos){
+
+    }
 }
--- lab06/src/mc322/lab06/Wumpus.java
+++ lab06/src/mc322/lab06/Wumpus.java
@@ -1,5 +1,39 @@
 package mc322.lab06;
 
-public class Wumpus extends Componente {
+import java.util.Random;
 
+public class Wumpus extends Componente {
+    public static int prioridade = 4;
+    private boolean alive;
+
+    Wumpus(String sprite, int score) {
+        super.Componentes("W", -1000);
+        this.alive = true;
+    }
+
+    public getAlive(){
+        return this.alive;
+    }
+
+    public boolean receiveDamage(){
+        Random dano = new Random(1);
+        if(dano == 0){
+            die();
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int dealDamage(){
+        return getValue();
+    }
+
+    public void die(){
+        this.alive = false;
+    }
+
+    public void gerarFedor(Caverna caverna, int []pos){
+        
+    }
 }
